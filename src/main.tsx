c513fac7443be0a638c4d4743d882b0d362b57c4
holly bishop <hollybish10@hotmail.co.uk>
2025-05-01 17:26:02 +0100

    Reached & Completed 12 - Paginated Queries

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,35 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-).render(
+const queryClient = new QueryClient({
+  // // We can set the default options for all queries here
+  // defaultOptions: {
+  //   queries: {
+  //     // React Query will retry failed queries 3 times by default
+  //     retry: 3,
+  //     // If no component is using the data, it will be garbage collected (removed from the cache) after 5 minutes
+  //     cacheTime: 300_000, // 5 minutes#
+  //     // After 10 seconds of receiving the data it will be considered stale (outdated)
+  //     staleTime: 10 * 1000, // 10 seconds
+  //     // Defaults to true - disable refetch when going between tabs
+  //     refetchOnWindowFocus: false,
+  //     // Defaults to true - disable refetch when the browser window comes back online
+  //     refetchOnReconnect: false,
+  //     refetchOnMount: false,
+  //   },
+  // },
+});
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>
 );
---
+++ src/react-query/hooks/usePosts.ts
@@ -0,0 +1,34 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+interface PostQuery {
+  page: number;
+  pageSize: number;
+}
+
+// const usePosts = (userId: Number | undefined) => {
+const usePosts = (query: PostQuery) => {
+  const fetchPosts = () =>
+    axios
+      .get<
+        Post[]
+      >('https://jsonplaceholder.typicode.com/posts', { params: { _start: (query.page - 1) * query.pageSize, _limit: query.pageSize } })
+      .then((res) => res.data);
+
+  return useQuery<Post[], Error>({
+    //  /users/1/posts
+    queryKey: ['posts', query],
+    queryFn: fetchPosts,
+    staleTime: 1 * 60 * 1000, // 1 minute
+    // Get a nicer user experience when switching between pages
+    keepPreviousData: true,
+  });
+};
+export default usePosts;
