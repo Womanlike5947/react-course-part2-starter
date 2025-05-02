1f6fd7ea1fb68885555f20c7c05203cbdcbd6921
holly bishop <hollybish10@hotmail.co.uk>
2025-05-02 11:58:45 +0100

    Returned pages in usePosts

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -4,17 +4,8 @@ import usePosts from './hooks/usePosts';
 const PostList = () => {
   const pageSize = 10;
 
-  // const [userId, setUserId] = useState<number>();
-
-  // const { data: posts, error, isLoading } = usePosts(userId);
-
-  const {
-    data: posts,
-    error,
-    isLoading,
-    fetchNextPage,
-    isFetchingNextPage,
-  } = usePosts({ pageSize });
+  const { data, error, isLoading, fetchNextPage, isFetchingNextPage } =
+    usePosts({ pageSize });
 
   if (isLoading) return <p>Loading...</p>;
 
@@ -22,17 +13,8 @@ const PostList = () => {
 
   return (
     <>
-      {/* <select
-        value={userId}
-        onChange={(event) => setUserId(parseInt(event.target.value))}
-        className="form-select mb-3">
-        <option value="">Select a user</option>
-        <option value="1">User 1</option>
-        <option value="2">User 2</option>
-        <option value="3">User 3</option>
-      </select> */}
       <ul className="list-group">
-        {posts?.pages.map((page, index) => (
+        {data.pages.map((page, index) => (
           <Fragment key={index}>
             {page.map((post) => (
               <li key={post.id} className="list-group-item">
@@ -42,13 +24,6 @@ const PostList = () => {
           </Fragment>
         ))}
       </ul>
-      {/* me-3 === marginEnd = 3 */}
-      {/* <button
-        className="btn btn-primary my-3 me-3"
-        onClick={() => setPage(page - 1)}
-        disabled={page === 1}>
-        Previous
-      </button> */}
       <button
         className="btn btn-primary my-3"
         onClick={() => fetchNextPage()}
--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -9,30 +9,23 @@ interface Post {
 }
 
 interface PostQuery {
-  // page: number;
   pageSize: number;
 }
 
-// const usePosts = (userId: Number | undefined) => {
 const usePosts = (query: PostQuery) => {
-  // const fetchPosts = () =>
-  //   axios
-  //     .get<
-  //       Post[]
-  //     >('https://jsonplaceholder.typicode.com/posts', { params: { _start: (query.page - 1) * query.pageSize, _limit: query.pageSize } })
-  //     .then((res) => res.data);
-
   return useInfiniteQuery<Post[], Error>({
     //  /users/1/posts
     queryKey: ['posts', query],
     // queryFn: fetchPosts,
     queryFn: ({ pageParam = 1 }) =>
-      axios.get('https://jsonplaceholder.typicode.com/posts', {
-        params: {
-          _start: (pageParam - 1) * query.pageSize,
-          _limit: query.pageSize,
-        },
-      }),
+      axios
+        .get('https://jsonplaceholder.typicode.com/posts', {
+          params: {
+            _start: (pageParam - 1) * query.pageSize,
+            _limit: query.pageSize,
+          },
+        })
+        .then((res) => res.data),
     staleTime: 1 * 60 * 1000, // 1 minute
     // Get a nicer user experience when switching between pages
     keepPreviousData: true,
