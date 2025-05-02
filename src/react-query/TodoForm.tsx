4f5fafa33ec47516ed89882d224156216b5be0a6
holly bishop <hollybish10@hotmail.co.uk>
2025-05-02 14:28:01 +0100

    17 - Showing Mutation Progress

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -13,20 +13,13 @@ const TodoForm = () => {
         .then((res) => res.data),
     onSuccess: (savedTodo, newTodo) => {
       console.log(savedTodo);
-      // APPROACH: Invalidating the code
-      // queryClient.invalidateQueries({
-      //   queryKey: ['todos'],
-      // });
 
-      // APPROACH 2: Updating the data in the cache
       queryClient.setQueryData<Todo[]>(['todos'], (todos) => [
         savedTodo,
         ...(todos || []),
       ]);
+      if (ref.current) ref.current.value = '';
     },
-
-    // onError
-    // onSettled
   });
   const ref = useRef<HTMLInputElement>(null);
 
@@ -52,7 +45,9 @@ const TodoForm = () => {
           <input ref={ref} type="text" className="form-control" />
         </div>
         <div className="col">
-          <button className="btn btn-primary">Add</button>
+          <button className="btn btn-primary" disabled={addTodo.isLoading}>
+            {addTodo.isLoading ? 'Adding...' : 'Add'}
+          </button>
         </div>
       </form>
     </>
