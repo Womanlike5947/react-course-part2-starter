5bc921030e8bb7b5a32552ca4e0ffca3ce6aa146
holly bishop <hollybish10@hotmail.co.uk>
2025-05-21 09:37:26 +0100

    Implemented Simple-Zustand-DevTools

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -3,9 +3,11 @@ import useCounterStore from './counter/store';
 
 const NavBar = () => {
   // #regions Context
-  const { counter } = useCounterStore();
-
+  const counter = useCounterStore((s) => s.counter);
   // #endregion
+
+  console.log('Rendering NavBar');
+
   return (
     <nav className="navbar d-flex justify-content-between">
       <span className="badge text-bg-secondary">{counter}</span>
--- src/state-management/counter/store.ts
+++ src/state-management/counter/store.ts
@@ -1,15 +1,24 @@
+import { mountStoreDevtool } from 'simple-zustand-devtools';
 import { create } from 'zustand';
 
 interface CounterStore {
   counter: number;
+  max: number;
   increment: () => void;
   reset: () => void;
 }
 
 const useCounterStore = create<CounterStore>((set) => ({
   counter: 0,
+  max: 5,
   increment: () => set((store) => ({ counter: store.counter + 1 })),
-  reset: () => set(() => ({ counter: 0 })),
+  reset: () => set(() => ({ max: 10 })),
 }));
+
+// process is an node global variable
+// process.env.NODE_ENV is set to 'development' or 'production' based on the environment
+if (process.env.NODE_ENV === 'development') {
+  mountStoreDevtool('Counter Store', useCounterStore);
+}
 
 export default useCounterStore;
