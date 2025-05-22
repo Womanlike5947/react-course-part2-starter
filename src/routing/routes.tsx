9b4d2048c8614726929adfc0dd79338fbd4c6f1b
holly bishop <hollybish10@hotmail.co.uk>
2025-05-22 13:25:50 +0100

    9 - Handling Errors
    How to Handle Page Errors with React Router's inbuilt isRouteErrorResponse

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
+import ErrorPage from './ErrorPage';
 import HomePage from './HomePage';
 import Layout from './Layout';
 import UserDetails from './UserDetails';
@@ -8,6 +9,7 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
